4e9af481e915990df6ed02862e5bf1b497368897
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-07-09 06:17:51 +0300

    show modal bottom sheet

--- lib/notes_app/views/notes_view.dart
+++ lib/notes_app/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_udemy_tharwat2/notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:flutter_udemy_tharwat2/notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -8,10 +9,16 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => const AddNoteBottomSheet(),
+          );
+        },
         child: const Icon(Icons.add),
       ),
       body: const NotesViewBody(),
     );
   }
 }
+
--- lib/notes_app/views/widgets/notes_list_view.dart
+++ lib/notes_app/views/widgets/notes_list_view.dart
@@ -3,14 +3,35 @@ import 'package:flutter/material.dart';
 import 'custom_notes_item.dart';
 
 class NotesListView extends StatelessWidget {
-  const NotesListView({super.key,});
-  final List<Color> colors = const[const Color(0xffFFCC80), Color(0xffFFCCb0),Color(0xffFFCCe0),Color(0xffFFCC80), Color(0xffFFCCb0),Color(0xffFFCCe0),];
+  const NotesListView({
+    super.key,
+  });
+  final List<Color> colors = const [
+    const Color(0xffFFCC80),
+    Color(0xffFFCCb0),
+    Color(0xffFFCCe0),
+    Color(0xffFFCC80),
+    Color(0xffFFCCb0),
+    Color(0xffFFCCe0),
+    Color(0xffFFCC80),
+    Color(0xffFFCCb0),
+    Color(0xffFFCCe0),
+    Color(0xffFFCC80),
+    Color(0xffFFCCb0),
+    Color(0xffFFCCe0),
+  ];
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: colors.length,
-      itemBuilder: (context, index) =>  Padding(
-        padding: EdgeInsets.symmetric(vertical: 4.0),
-        child: NoteItem(color: colors[index],),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemCount: colors.length,
+        itemBuilder: (context, index) => Padding(
+          padding: EdgeInsets.symmetric(vertical: 4.0),
+          child: NoteItem(
+            color: colors[index],
+          ),
+        ),
       ),
     );
   }
