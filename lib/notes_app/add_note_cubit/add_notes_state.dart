5d3f857d1c240e1c97d15bfd4f125e0ca1bcbe4b
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-07-11 00:36:31 +0300

    create add note states

--- lib/notes_app/add_note_cubit/add_notes_state.dart
+++ lib/notes_app/add_note_cubit/add_notes_state.dart
@@ -4,3 +4,10 @@ part of 'notes_cubit.dart';
 abstract class AddNotesState{}
 
 class AddNotesInitial extends AddNotesState{}
+class AddNotesLoading extends AddNotesState{}
+class AddNotesSuccess extends AddNotesState{}
+class AddNotesFailure extends AddNotesState{
+  final String errMessage;
+
+  AddNotesFailure(this.errMessage);
+}
