e25eabd6ea32d4ad9c9a1fe2615df61b2a1c3743
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-07-15 01:41:55 +0300

    create notes cubit

---
+++ lib/notes_app/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import '../../constants.dart';
+import '../../models/note_model.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes() async {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = notesBox.values.toList();
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
+}
