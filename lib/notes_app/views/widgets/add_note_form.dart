6f02d8e425302249e656cfbebe41cd5958327e9e
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-07-19 04:50:22 +0300

    format date time

--- lib/notes_app/views/widgets/add_note_form.dart
+++ lib/notes_app/views/widgets/add_note_form.dart
@@ -59,7 +59,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
                     var noteModel = NoteModel(
                       title: title!,
                       subTitle: subTitle!,
-                      date: formattedCurrentDate.toString(),
+                      date: formattedCurrentDate,
                       color: Colors.blue.value,
                     );
                     BlocProvider.of<AddNotesCubit>(context).addNote(noteModel);
