053dae167314883f6eac8367cab7155ffaf0cef1
basmalahesham <112768821+basmalahesham@users.noreply.github.com>
2024-07-22 04:14:29 +0300

    create colors list part two

--- lib/notes_app/views/widgets/colors_list_view.dart
+++ lib/notes_app/views/widgets/colors_list_view.dart
@@ -2,8 +2,24 @@ import 'package:flutter/material.dart';
 
 import 'color_item.dart';
 
-class ColorsListView extends StatelessWidget {
-  const ColorsListView({super.key});
+class ColorsListView extends StatefulWidget {
+  const ColorsListView({
+    super.key,
+  });
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  List<Color> colors = const [
+    Color(0xffEF7C8E),
+    Color(0xffFAE8E0),
+    Color(0xffB6E2D3),
+    Color(0xffD8A7B1),
+    Color(0xfff695ad),
+  ];
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +27,19 @@ class ColorsListView extends StatelessWidget {
       height: 38 * 2,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) => const ColorItem(),
+        itemCount: colors.length,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 6.0),
+          child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: colors[index],
+                isActive: currentIndex == index,
+              )),
+        ),
       ),
     );
   }
